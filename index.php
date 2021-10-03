5f4f888bedb32a90e6865ded321b0205e64a4c20
Chirantan Banerjee <65519645+ChirantanTech@users.noreply.github.com>
2021-10-03 18:25:45 +0530

    Updated index.php
    Some microsoft edge browser version don't support Hex colour so since error class is important in these project i have changed its colour property to red.

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>SS CONSULTANCY</title>
+		<title>SS CONSULTANCY BALASORE</title>
 		<meta name="keywords" content="Tax consultancy in balasore,Gst balasore,best tax consultancy firm in
 		odisha,best tax consultancy firm in balasore,gst,gst balasore,tax service"/>
 		<!-- Style Error -->
 		<style>
-		.error {color: #FF0000;}
+		.error {color: red;}
 		</style>
 		<?php include 'links.php'?>
 	</head>
